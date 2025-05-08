8239f799f5d950dfc8b279f60bb35527cab9d52b
Fredy28 <fredinavarrete19@gmail.com>
2025-05-08 15:05:51 -0600

    Cambios Fredi=tabla registros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { RegistrosComponent } from './pages/registros/registros.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'registros',
+        component: RegistrosComponent
+    }
+];
---
+++ src/app/pages/registros/registros.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-registros',
+  imports: [RouterModule, CommonModule],
+  templateUrl: './registros.component.html',
+  styleUrl: './registros.component.css'
+})
+export class RegistrosComponent {
+
+}
