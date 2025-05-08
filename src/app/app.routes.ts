64e926f19efab5ce92e3e4a1bc3f7275cdacde9f
Castellml <107086379+Castellml@users.noreply.github.com>
2025-05-08 16:08:14 -0600

    Mis cambios 3  Amanda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import path from 'path';
 import { RegistrosComponent } from './pages/registros/registros.component';
 import { CargarVideoComponent } from './pages/cargar-video/cargar-video.component';
+import { DeteccionComponent } from './pages/deteccion/deteccion.component';
 
 export const routes: Routes = [
     {
@@ -11,5 +12,7 @@ export const routes: Routes = [
     {
         path: 'cargar-video',
         component: CargarVideoComponent
-    }
+    },
+    {   path: 'deteccion', 
+        component: DeteccionComponent }
 ];
---
+++ src/app/pages/deteccion/deteccion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deteccion',
+  imports: [],
+  templateUrl: './deteccion.component.html',
+  styleUrl: './deteccion.component.css'
+})
+export class DeteccionComponent {
+
+}
