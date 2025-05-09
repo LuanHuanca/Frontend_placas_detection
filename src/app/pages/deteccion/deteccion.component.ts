57ea84e9aca7999e497174c60074360401b5f808
Rafael Ascoltare <Rafael Ascoltare.014421825@ulsaoaxaca.edu.mx>
2025-05-08 22:58:00 -0600

    Merge remotos en develop

--- src/app/pages/deteccion/deteccion.component.ts
+++ src/app/pages/deteccion/deteccion.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-
+import { Router } from '@angular/router';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 @Component({
   selector: 'app-deteccion',
   imports: [NavbarComponent, FooterComponent],
   templateUrl: './deteccion.component.html',
-  styleUrl: './deteccion.component.css'
+  styleUrl: './deteccion.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class DeteccionComponent {
-
+  constructor(private router: Router) {}
+  goHome() {
+    this.router.navigate(['/']);
+  }
 }
