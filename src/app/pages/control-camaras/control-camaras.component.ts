a6c2324dc25fc43b2e1f244c07173d88c4526d1d
LuanHuanca <luanhuancam@gmail.com>
2025-05-09 09:41:57 -0600

    connection for image detection

--- src/app/pages/control-camaras/control-camaras.component.ts
+++ src/app/pages/control-camaras/control-camaras.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CamarasService, Camara } from '../../services/camaras.service';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-control-camaras',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NavbarComponent, FooterComponent],
   templateUrl: './control-camaras.component.html',
   styleUrls: ['./control-camaras.component.css']
 })
